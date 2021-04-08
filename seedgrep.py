b1ab1a994ad5c47edd29e082a804a4178409631e
polymetric <polymetricofficial@gmail.com>
2021-04-08 08:12:19 -0400

    dump seeds for later filtering instead of filtering initially

--- seedgrep.py
+++ seedgrep.py
@@ -4,21 +4,30 @@ import os
 import argparse
 from nbt.nbt import *
 
-parser = argparse.ArgumentParser(description="Search for Minecraft level.dats with a certain seed.", allow_abbrev=True)
-parser.add_argument("-seed", metavar="seed", required=True, type=int, help="the seed to search for")
-parser.add_argument("-root", metavar="root", default=".", type=str, help="the root directory to walk")
-
+parser = argparse.ArgumentParser(description="Search for Minecraft level.dats and dump the seeds in them.", allow_abbrev=True)
+parser.add_argument("-out", metavar="output file", default="seeds.txt", type=str, help="file to dump seeds to")
+parser.add_argument("-dir", metavar="dir", default=".", type=str, help="the root directory to walk")
+parser.add_argument("-verbose", default=False, action='store_true', help="print every seed to the console")
 args = parser.parse_args()
 
-targetseed = args.seed
+outfile = open(args.out, "a")
+absroot = os.path.abspath(args.dir)
+worldsfound = 0
 
-for root, dirs, files in os.walk(args.root):
+print("walking directory {}...".format(absroot))
+
+for root, dirs, files in os.walk(absroot):
     for file in files:
         if file != "level.dat":
             continue
+        file = os.path.join(root, file)
+        worldsfound += 1
 #       print("found level.dat file {}, checking...".format(file))
         nbtfile = NBTFile(file, "rb")
         fileseed = nbtfile["Data"]["RandomSeed"].value
-        if fileseed == targetseed:
-            print("found target seed {} in {}".format(targetseed, os.path.join(root, file)))
+        if args.verbose:
+            print("found seed {} in {}".format(fileseed, file))
+        outfile.write("{}: {}\n".format(file, fileseed))
+
+print("done. found {} total worlds.".format(worldsfound))
 
