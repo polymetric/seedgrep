2362993dd4a00c523b70e636b3d080899639da90
polymetric <polymetricofficial@gmail.com>
2021-04-08 08:39:20 -0400

    error checks

--- seedgrep.py
+++ seedgrep.py
@@ -2,6 +2,7 @@
 
 import os
 import argparse
+import traceback
 from nbt.nbt import *
 
 parser = argparse.ArgumentParser(description="Search for Minecraft level.dats and dump the seeds in them.", allow_abbrev=True)
@@ -10,6 +11,7 @@ parser.add_argument("-dir", metavar="dir", default=".", type=str, help="the root
 parser.add_argument("-verbose", default=False, action='store_true', help="print every seed to the console")
 args = parser.parse_args()
 
+# TODO make this overwrite the output file, with a catch warning
 outfile = open(args.out, "a")
 absroot = os.path.abspath(args.dir)
 worldsfound = 0
@@ -18,16 +20,25 @@ print("walking directory {}...".format(absroot))
 
 for root, dirs, files in os.walk(absroot):
     for file in files:
-        if file != "level.dat":
+        if file != "level.dat" and file != "level.dat_old":
             continue
-        file = os.path.join(root, file)
         worldsfound += 1
-#       print("found level.dat file {}, checking...".format(file))
-        nbtfile = NBTFile(file, "rb")
-        fileseed = nbtfile["Data"]["RandomSeed"].value
-        if args.verbose:
-            print("found seed {} in {}".format(fileseed, file))
-        outfile.write("{}: {}\n".format(file, fileseed))
+        file = os.path.join(root, file)
+        try:
+            nbtfile = NBTFile(file, "rb")
+            fileseed = nbtfile["Data"]["RandomSeed"].value
+            if args.verbose:
+                print("found seed {} in {}".format(fileseed, file))
+            outfile.write("{}: {}\n".format(file, fileseed))
+        except Exception as e:
+            if args.verbose:
+                # TODO make this actually handle the error properly.
+                # a lot of these errors seem to come from level.dats which
+                # are missing the seed tag. probably because it was changed at some point
+                pass
+#               print("error occurred on file {}:".format(file))
+#               print(traceback.format_exc())
+#               print(e)
 
 print("done. found {} total worlds.".format(worldsfound))
 
